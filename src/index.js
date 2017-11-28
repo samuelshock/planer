91047fb46cc847093539835cb797a159f40eacb0
samuel choque <samuelchq418@gmail.com>
2017-11-27 20:17:28 -0400

    Add structure login

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import CelebrityJokes from './components/CelebrityJokes';
+import FoodJokes from './components/FoodJokes';
+import { Router, Route } from 'react-router';
+import history from './history';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const Root = () => {
+  return (
+    <div className="container">
+      <Router history={history}>
+	  	<div>
+        	<Route path="/" component={FoodJokes}/>
+        	<Route path="/special" component={CelebrityJokes}/>
+		</div>
+      </Router>
+    </div>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
